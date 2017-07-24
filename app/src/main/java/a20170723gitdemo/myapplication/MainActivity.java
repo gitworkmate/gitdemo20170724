8b149f90ba6fdd348353780fece21a0aaae46b44
houwenhao4147 <houwenhao4147@163.com>
2017-07-25 00:54:35 +0800

    Merge remote-tracking branch 'origin/dev2' into dev
    # Conflicts:
    #	app/src/main/java/a20170723gitdemo/myapplication/MainActivity.java

--- app/src/main/java/a20170723gitdemo/myapplication/MainActivity.java
+++ app/src/main/java/a20170723gitdemo/myapplication/MainActivity.java
@@ -60,7 +60,6 @@ public class MainActivity extends AppCompatActivity {
         Log.e("TAG","MainActivitydevgitdev");
 
 
-
 		  Log.e("TAG","MainActivitydevgit---dev");
         Log.e("TAG","MainActivitydevgit---dev23");
     }
